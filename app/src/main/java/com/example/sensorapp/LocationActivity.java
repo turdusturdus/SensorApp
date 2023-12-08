28edf077a6505a7dbaeca026da3c221a97208d71
Łukasz Kowalewski <l.kowalewski.gaid@gmail.com>
2023-12-08 23:52:56 +0100

    Set up localisation displaying

--- app/src/main/java/com/example/sensorapp/LocationActivity.java
+++ app/src/main/java/com/example/sensorapp/LocationActivity.java
@@ -6,37 +6,54 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.Manifest;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class LocationActivity extends AppCompatActivity {
 
     private static final int REQUEST_LOCATION_PERMISSION = 1;
     private static final String TAG = "LocationActivity";
+    private Location lastLocation;
+    private TextView locationTextView;
+    private FusedLocationProviderClient fusedLocationClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        locationTextView = findViewById(R.id.textview_location);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
         Button getLocationButton = findViewById(R.id.button_location);
         getLocationButton.setOnClickListener(v -> getLocation());
     }
 
     private void getLocation() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{
-                            Manifest.permission.ACCESS_FINE_LOCATION},
-                    REQUEST_LOCATION_PERMISSION);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
         } else {
-            Log.d(TAG, "getLocation: permissions granted");
+            fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                if (location != null) {
+                    lastLocation = location;
+                    locationTextView.setText(getString(R.string.location_text, location.getLatitude(), location.getLongitude(), location.getTime()));
+                } else {
+                    locationTextView.setText(R.string.no_location);
+                }
+            });
         }
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
